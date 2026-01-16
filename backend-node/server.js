854156110cdb5a078d39b9cffa9df0cabd5ec39e
ruchitparmar11 <ruchitparmar78@gmail.com>
2026-01-16 16:13:11 +0530

    Localhost

--- backend-node/server.js
+++ backend-node/server.js
@@ -8,10 +8,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
